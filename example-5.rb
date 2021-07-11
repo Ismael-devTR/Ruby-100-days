0c27bb6337315df4cb0189f5871a8d52c75bc7f1
Ismael-devTR <dev.tristan.romero@gmail.com>
2021-07-11 16:49:03 -0500

    example five

---
+++ example-5.rb
@@ -0,0 +1,11 @@
+#Get rid of 11. And append a 3.
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+
+arr.pop  # This remove the last item inside the Array 
+print arr
+
+#arr << 3
+arr.push(3)
+print arr
